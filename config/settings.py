e3a86ab71c7a01a58cabe52eb40b55d09c81b8fc
Artem <baza.a1@bk.ru>
2024-07-20 00:47:06 +0300

    Переделал подход к STATIC_URL и MEDIA_ROOT

--- config/settings.py
+++ config/settings.py
@@ -102,14 +102,9 @@ USE_TZ = True
 
 
 STATIC_URL = 'static/'
-STATICFILES_DIR = (
-    BASE_DIR / 'static/'
-)
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = (
-    BASE_DIR / 'media/'
-)
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
